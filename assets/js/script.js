7a44df7fe25f071316289624df557d6e3ddbe10f
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-19 14:50:33 -0500

    tasks are now added with a type and the form resets after each task creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,32 @@
-
-var buttonEl = document.querySelector("#save-task"); // points button
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //points to ul
 var formEl = document.querySelector("#task-form");
 
-var createTaskHandler = function(event) {
-
+var taskFormHandler = function(event) {
   event.preventDefault();
 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  if(!taskNameInput || !taskTypeInput){
+    alert("You need to fill out the task form!");
+  }
+  else{
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput,
+    };
+    createTaskEl(taskDataObj);
+ }
+ formEl.reset();
+};
+
+var createTaskEl = function(taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
   tasksToDoEl.appendChild(listItemEl);
-};
-formEl.addEventListener("submit", createTaskHandler);
-buttonEl.addEventListener("click", createTaskHandler); // as click happens, runs function
+}
+
+formEl.addEventListener("submit", taskFormHandler);
